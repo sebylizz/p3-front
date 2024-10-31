98eb4cb8d9cba0d12f87203016a12363d374fd79
sebylizz <sebastiankjandersen@gmail.com>
2024-10-31 09:37:48 +0100

    Optimized sub-optimal code

--- src/app/importProducts.js
+++ src/app/importProducts.js
@@ -1,31 +1,26 @@
-function productFetcher() {
-    return fetch("https://kjaeldgaard.com/leghetto/products/getproducts")
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
+async function productFetcher() {
+    try {
+        const response = await fetch("http://localhost:8080/products/getproducts");
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        let products = [];
+        for (let i = 0; i < data.length; i++) {
+            let product = {};
+            product.id = data[i].id;
+            product.name = data[i].name;
+            product.size = data[i].size;
+            product.price = data[i].price;
+            product.img = data[i].image || "placeholder.jpg";
+            products.push(product);
+        }
 
-            let products = [];
-            for (let i = 0; i < data.length; i++) {
-                let product = {};
-                product.id = data[i].id;
-                product.name = data[i].name;
-                product.size = data[i].size;
-                product.price = data[i].price;
-                product.img = data[i].image || "placeholder.jpg";
-                products.push(product);
-            }
-
-            console.log(products);
-            return products;
-
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
+        console.log(products);
+        return products;
+    } catch (error) {
+        console.error('Error:', error);
+    }
 }
 
 export default productFetcher;
