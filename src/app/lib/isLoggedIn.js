d3dea0e8738bca0d54633f36f32b77048714ef69
LukasTN03 <DL48WD@student.aau.dk>
2024-11-11 12:15:18 +0100

    now also checks for expired token

--- src/app/lib/isLoggedIn.js
+++
@@ -1,8 +0,0 @@
-'use server'
-import { cookies } from 'next/headers'
-
-export default async function isLoggedIn() {
-    const cookieStore = await cookies()
-    const hasCookie = cookieStore.has('token')
-    return hasCookie
-}
