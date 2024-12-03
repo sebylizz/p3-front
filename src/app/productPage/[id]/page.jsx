63bb3732aa1c770ec912fba404edc30f41f26f7b
emilkjeldgaard <ekkr23@student.aau.dk>
2024-12-03 11:35:54 +0100

    elsker farver

--- src/app/productPage/[id]/page.jsx
+++ src/app/productPage/[id]/page.jsx
@@ -115,7 +115,12 @@ export default function ProductDetailsPage() {
                     description = {product.description}
                 />
                 <SizeSelector sizes={sizes} onSizeSelect={setSelectedSize} colorIndex={currentColorIndex} colors={product.colors} />
-                <AddToCartButton productId={product.id} colorId={product.colors[currentColorIndex].id} selectedSize={selectedSize} />
+                <AddToCartButton 
+                    productId={product.id} 
+                    colorId={product.colors[currentColorIndex].id} 
+                    selectedSize={selectedSize} 
+                    products={products} 
+                />
                 <button
                     onClick={() => setShowDescription(!showDescription)}
                     className={`w-full p-4 mt-4 text-white ${showDescription ? 'bg-black' : 'bg-gray-400'}`}
